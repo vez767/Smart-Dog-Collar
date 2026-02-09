31abed4885021e09461ddb501cf491352523a532
vez767 <wa3960f@greenwich.ac.uk>
2026-02-09 11:36:03 +0000

    Initialize dog on stack and implement setup logic

--- dog_collar.c
+++ dog_collar.c
@@ -6,19 +6,31 @@ unsigned char collar_state = 0;
 #define LED_PIN (1<<0)
 #define SPEAKER_PIN (1<<1)
 #define VIB_PIN (1<<2)
-#define ALARM_PIN (1<<3)
-
-
+#define ALARM_PIN (1<<3) 
 
 struct Dog{
     char name[20];
     float distance_from_home;
     float limit;
 };
+void setup_dog(struct Dog *d){
+    printf("\t\tDOG INITIALS MENU\n\n");
 
+    printf("Enter pet's name :-)\n");
+    printf("---> ");
+    scanf("%s", d->name);
+    printf("\n\t\tName save successful..\n\n");
 
+    printf("Set maximum boundary distance (meters)");
+    printf("---> ");
+    scanf("%f", &d->limit);
 
+    d->distance_from_home = 0.0;
+
+    printf("\n\t\tBoundary confirmed. %s's tag is set to go...\n", d->name);
+}
 int main(){
-    printf("Firmware Booting..\n");
-    return 0;
+ struct Dog my_dog;
+ setup_dog(&my_dog);
+ return 0;
 }
